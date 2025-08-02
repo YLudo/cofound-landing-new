eafe0a018bb5c2714b800ba7a4d7fe476694909a
DevLudo <roux.ludovic.pro@gmail.com>
2025-08-02 21:06:56 +0200

    feat: add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,27 @@ const fontSans = FontSans({
     subsets: ["latin"],
 });
 
+const siteUrl = "https://www.cofound-app.com/"
+
 export const metadata: Metadata = {
     title: "CoFound",
-    description: "L'écosystème de co-création",
+    description: "La plateforme qui connecte porteurs de projets, développeurs, designers et autres. Trouvez vos co-fondateurs grâce à notre matching IA et donnez vie à vos idées de startup.",
+    openGraph: {
+        title: "CoFound : Trouvez vos associés et lancez votre projet",
+        description: "La plateforme pour transformer vos idées en startups à succès.",
+        url: siteUrl,
+        siteName: "CoFound",
+        images: [
+            {
+                url: `${siteUrl}/logo.png`, 
+                width: 1200,
+                height: 630,
+                alt: "Illustration de la plateforme CoFound",
+            },
+        ],
+        locale: "fr_FR",
+        type: "website"
+    }
 };
 
 export default function RootLayout({
