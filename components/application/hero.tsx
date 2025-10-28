709ebcf4b96389a02383c3d44d1a8b6d96647b6e
DevLudo <roux.ludovic.pro@gmail.com>
2025-10-28 23:48:29 +0100

    feat: :sparkles: add hamburger menu

--- components/application/hero.tsx
+++ components/application/hero.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 
 export default function Hero() {
     return (
-        <section className="min-h-screen gap-32 py-8 bg-primary flex flex-col justify-between">
+        <section className="min-h-screen gap-24 py-8 bg-primary flex flex-col justify-between">
             <div className="container mx-auto px-4 md:px-8">
-                <nav className="flex items-center justify-between">
+                <nav className="hidden md:flex items-center justify-between">
                     <div className="flex items-center gap-6">
                         <Link href="/" className="flex items-center gap-2 text-white">
                             <img src={"/logo_icon.svg"} alt="Logo de CoFound" className="max-h-12" />
                             <span className="text-lg font-semibold tracking-tighter">CoFound</span>
                         </Link>
                     </div>
-                    <div className="flex gap-2">
+                    <div className="hidden md:flex gap-2">
                         <Button variant="ghost" className="text-white border-white hover:bg-white hover:text-primary" asChild>
                             <Link href={`${process.env.NEXT_PUBLIC_DASHBOARD_URL}/login`}>
                                 Se connecter
@@ -27,6 +28,47 @@ export default function Hero() {
                         </Button>
                     </div>
                 </nav>
+                <div className="block md:hidden">
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-6">
+                            <Link href="/" className="flex items-center gap-2 text-white">
+                                <img src={"/logo_icon.svg"} alt="Logo de CoFound" className="max-h-12" />
+                                <span className="text-lg font-semibold tracking-tighter">CoFound</span>
+                            </Link>
+                        </div>
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button variant="outline" size="icon" className="cursor-pointer">
+                                    <Menu className="size-4" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent className="bg-primary border-primary overflow-y-auto">
+                                <SheetHeader>
+                                    <SheetTitle>
+                                        <Link href="/" className="flex items-center gap-2 text-white">
+                                            <img src={"/logo_icon.svg"} alt="Logo de CoFound" className="max-h-8" />
+                                            <span className="text-lg font-semibold tracking-tighter">CoFound</span>
+                                        </Link>
+                                    </SheetTitle>
+                                </SheetHeader>
+                                <div className="flex flex-col gap-6 p-4">
+                                    <div className="flex flex-col gap-4">
+                                        <Button variant="secondary">
+                                            <Link href={`${process.env.NEXT_PUBLIC_DASHBOARD_URL}/register`}>
+                                                Nous rejoindre
+                                            </Link>
+                                        </Button>
+                                        <Button variant="ghost" className="text-white border-white hover:bg-white hover:text-primary" asChild>
+                                            <Link href={`${process.env.NEXT_PUBLIC_DASHBOARD_URL}/login`}>
+                                                Se connecter
+                                            </Link>
+                                        </Button>
+                                    </div>
+                                </div>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+                </div>
             </div>
             <div className="container mx-auto px-4 md:px-8">
                 <div className="grid items-center gap-8 lg:grid-cols-2">
